456a711630f13e47400bc449fbc64c1eb40f8454
Muhammad Bilal Riaz <briaz0555@gmail.com>
2024-09-05 20:47:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+//Question #01
+
+//var city = prompt("Enter the city name");
+//if(city =="karachi");
+//console.log("welcome to the city of lights");
+
+
+// Question #02
+
+// var gender=prompt("Enter your gender")
+// if(gender =="male")
+//     console.log("Good Morning Sir");
+// if(gender =="female")
+//     console.log("Good Mornig Ma'am");
+
+
+//Question #03
+
+// var signal = prompt("Select the signal color: \n Red \n Yellow \n Green");
+// if (signal == "red");
+// console.log("Must Stop");
+// if (signal == "yellow")
+//     console.log("Ready to move");
+// if(signal =="green");
+//     console.log("Move now");
+
+
+
+//Question #04
+var fuel = prompt("input remaining fuel");
+if (fuel < 0.25)
+    console.log("Please Refill the fuel in your Car!");
+    
+    
+
+
+
+
+
+
